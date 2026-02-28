d688f34f7b485e50790bcc07f4e73fb677c91970
XpnsiveSharks <aban.marynelle.tesoro@gmail.com>
2026-02-28 12:17:55 +0800

    chore: remove empty stub RegisterActivity from ui/auth package

--- app/src/main/java/com/example/ucms_android/ui/auth/RegisterActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.ucms_android.ui.auth;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class RegisterActivity extends AppCompatActivity {
-    // TODO: implement screen
-}
